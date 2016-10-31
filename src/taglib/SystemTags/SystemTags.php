74b6efb77a5f340923b33f7c9ad7af52eb096943
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2016-10-31 10:10:47 +0200

    Update SystemTags.php

--- src/taglib/SystemTags/SystemTags.php
+++ src/taglib/SystemTags/SystemTags.php
@@ -3,7 +3,6 @@ require_once("SystemEscapeTag.php");
 require_once("SystemImportTag.php");
 require_once("SystemScriptTag.php");
 require_once("SystemStyleTag.php");
-require_once("SystemPHPTag.php");
 
 /**
  * Envelopes system tags into a backup-restore paradigm
@@ -18,7 +17,6 @@ class SystemTags {
 		$this->objEscapeTag = new SystemEscapeTag();
 		$this->objScriptTag = new SystemScriptTag();
 		$this->objStyleTag = new SystemStyleTag();
-		$this->objPHPTag = new SystemPHPTag();
 	}
 	
 	/**
@@ -30,7 +28,6 @@ class SystemTags {
 		if($this->objEscapeTag->hasContent($strOutputStream)) $this->objEscapeTag->removeContent($strOutputStream);
 		if($this->objScriptTag->hasContent($strOutputStream)) $this->objScriptTag->removeContent($strOutputStream);
 		if($this->objStyleTag->hasContent($strOutputStream))  $this->objStyleTag->removeContent($strOutputStream);
-		if($this->objPHPTag->hasContent($strOutputStream)) 	  $this->objPHPTag->removeContent($strOutputStream);
 	}
 	
 	/**
@@ -42,6 +39,5 @@ class SystemTags {
 		if($this->objEscapeTag->hasContent($strOutputStream)) $this->objEscapeTag->restoreContent($strOutputStream);
 		if($this->objScriptTag->hasContent($strOutputStream)) $this->objScriptTag->restoreContent($strOutputStream);
 		if($this->objStyleTag->hasContent($strOutputStream))  $this->objStyleTag->restoreContent($strOutputStream);
-		if($this->objPHPTag->hasContent($strOutputStream)) 	  $this->objPHPTag->restoreContent($strOutputStream);
 	}
 }
