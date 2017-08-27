53aee5c98c193abf08ec83ab975d69b75beab571
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-08-27 09:18:02 +0300

    + made user tags procedural

--- tests/taglib/User/UserInfoTag.php
+++
@@ -1,7 +0,0 @@
-<?php
-class UserInfoTag extends AbstractTag implements StartTag {
-	public function parseStartTag($parameters=array()) {
-		$this->checkParameters($parameters, array("id"));
-		return 'Name: ${names.'.$parameters["id"].'}<br/>';
-	}
-}
