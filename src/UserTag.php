7d962aa865ba4db64b43ca1ff727313636d8c2da
Lucian Gabriel Popescu <luciangreentree@github.com>
2017-11-14 18:00:30 +0200

    bugfix

--- src/UserTag.php
+++ src/UserTag.php
@@ -1,12 +1,8 @@
 <?php
 /**
- * Defines a generic class to parse all user-defined procedural tags under following rules:
- * - start and end tag bodies are separated using: <separator/>
- * - start tag (mandatory) body will be evaluated
- * - end tag (optional) body will be taken as text (not evaluated)
+ * Defines a generic class to parse all user-defined procedural tags.
  */
-class UserTag implements StartEndTag {
-	const BODY_TAG = "<separator/>";
+class UserTag implements StartTag {
 	private $strFilePath;
 	
 	/**
@@ -22,26 +18,8 @@ class UserTag implements StartEndTag {
 	 */
 	public function parseStartTag($parameters=array()) {
 		$strContent= file_get_contents($this->strFilePath);
-		$position = strpos($strContent,self::BODY_TAG);
-		if($position!==false) {
-			$strContent = substr($strContent, 0, $position);
-		}
 		return preg_replace_callback("/[\$]\[([a-zA-Z]+)\]/",function($match) use($parameters){
 			return (isset($parameters[$match[1]])?$parameters[$match[1]]:null);
 		},$strContent);
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 * @see StartEndTag::parseEndTag()
-	 */
-	public function parseEndTag() {
-		$strContent = file_get_contents($this->strFilePath);
-		$position = strpos($strContent,self::BODY_TAG);
-		if($position!==false) {
-			return substr($strContent, $position+strlen(self::BODY_TAG));
-		} else {
-			return "";
-		}
 	}
 }
