7064f621f7f20ed3f8831b36dd21a0bde349c5b8
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-09-01 14:54:19 +0300

    bugfix

--- src/taglib/System/SystemEscapeTag.php
+++ src/taglib/System/SystemEscapeTag.php
@@ -42,7 +42,7 @@ class SystemEscapeTag {
         }
         
         // restore content of escape tags
-        return preg_replace_callback("/\<bkp\>(.*?)\<\/bkp\>/si", function($matches) use($escaper) {
+        return preg_replace_callback("/\<bkp\>(.*?)\<\/bkp\>/si", function($matches) {
             return $this->matches[$matches[1]];
         }, $subject);
     }
