1966c7626220367d0088f9dc0629ed0ae5db7b48
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-05-22 11:18:40 +0300

    bugfix

--- src/taglib/Std/StdSetTag.php
+++ src/taglib/Std/StdSetTag.php
@@ -3,17 +3,17 @@ namespace Lucinda\Templating;
 
 /**
  * Implements how setting an internal variable is translated into a tag.
-*
-* Tag syntax:
-* <:set var="VARNAME" val="EXPRESSION"/>
-*/
+ *
+ * Tag syntax:
+ * <:set var="VARNAME" val="EXPRESSION"/>
+ */
 class StdSetTag extends SystemTag implements StartTag {
-	/**
-	 * {@inheritDoc}
-	 * @see StartTag::parseStartTag()
-	 */
-	public function parseStartTag($parameters=array()) {
-	    $this->checkParameters($parameters, array("var","val"));
-		return '<?php $'.$parameters['var'].' = '.($this->isExpression($parameters['val'])?$this->parseExpression($parameters['val']):"'".addslashes($parameters['val'])."'").'; ?>';
-	}
+    /**
+     * {@inheritDoc}
+     * @see StartTag::parseStartTag()
+     */
+    public function parseStartTag($parameters=array()) {
+        $this->checkParameters($parameters, array("var"));
+        return '<?php $'.$parameters['var'].' = '.(!empty($parameters['val'])?($this->isExpression($parameters['val'])?$this->parseExpression($parameters['val']):"'".addslashes($parameters['val'])."'"):"null").'; ?>';
+    }
 }
