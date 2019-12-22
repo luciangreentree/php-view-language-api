d1252a5063c69173a11bb24c0c751cb55dbf3738
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-12-22 20:42:19 +0200

    PSR4 compliance

--- src/TagExpressionParser.php
+++ src/TagExpressionParser.php
@@ -5,7 +5,7 @@ namespace Lucinda\Templating;
  * Implements scalar expressions found in parsable tag attribute values that are going to be interpreted as PHP when response is displayed to client.
  */
 class TagExpressionParser extends ExpressionParser
-{    
+{
     /**
      * For each macro-expression found, calls for its conversion to PHP and wraps it up as scriptlet.
      *
